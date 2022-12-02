2cd37885bb5f2d5be18e43d56126145020238b5c
MalaRuze <vojtech.ruzic@gmail.com>
2022-12-02 14:50:15 +0100

    heroku config removed

--- server/index.js
+++ server/index.js
@@ -26,13 +26,6 @@ app.get("/getTemplates", (req, res) => {
   });
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 app.listen(process.env.PORT, () => {
   console.log("Server started on port " + process.env.PORT);
 });
