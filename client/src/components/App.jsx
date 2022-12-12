15ed65019c7a069da3eadb26e3920ee36df5cbc4
MalaRuze <vojtech.ruzic@gmail.com>
2022-12-12 13:47:33 +0100

    webservice provider issue fixed

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   // "https://easyinvite-api.onrender.com/getTemplates"
   useEffect(() => {
-    Axios.get("fancy-brioche-7e61f0.netlify.app/getTemplates").then(
+    Axios.get("https://fancy-brioche-7e61f0.netlify.app/getTemplates").then(
       (response) => {
         setListOfTemplates(response.data);
       }
